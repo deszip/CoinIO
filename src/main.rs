3d3a61f9a4892c62dd9e423662342e0e8d4a0a8b
Deszip <raf.rafiki@gmail.com>
2015-06-11 01:58:09 +0300

    matching CLI args

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,69 @@ mod coin_api;
 static COIN_LOGIN: &'static str = "raf.rafiki@gmail.com";
 static COIN_PASSWORD: &'static str = "cfd32";
 
+
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
 
+    //let unwrapped_args: Vec<&str> = args.iter().map(|& ref x| println!("arg: {:?}", x));
+
+    enum ParsingState {
+        empty,
+        expect_login,
+        expect_password
+    }
+
+    let mut state: ParsingState = ParsingState::empty;
+    let mut login: &str = "";
+    let mut password: &str = "";
+
+    for arg in args.iter() {
+        match arg.as_ref() {
+            "-l" => {
+                state = ParsingState::expect_login;
+                println!("state: login");
+            },
+            "-p" => {
+                state = ParsingState::expect_password;
+                println!("state: password");
+            }
+            value @ _ => {
+                match state {
+                    ParsingState::expect_login => {
+                        state = ParsingState::empty;
+                        login = value;
+                    }
+                    ParsingState::expect_password => {
+                        state = ParsingState::empty;
+                        password = value;
+                    }
+                    ParsingState::empty => {
+                        println!("Skipping: {}", value)
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Login: {}, password: {}", login, password);
+
+    //let tail: Vec<&str> = args.tail().iter().map(|x| &x).collect();
+    //println!("tail: {:?}", args);
+
+/*
+    match tail {
+        [ref executable_path] => {
+            panic!("Called with no arguments. Nothing to do.");
+        }
+
+        _ => {}
+    }
+*/
+
+    /*
     let mut coin_api = CoinApi::new(COIN_LOGIN, COIN_PASSWORD);
 
     let categories_count = coin_api.categories_count();
@@ -35,6 +93,7 @@ fn main() {
 
     println!("Categories: {}", categories_count);
     println!("Expenses: {}", expense_count);
+    */
 
-    coin_api.create_expense(12, "Foo");
+    //coin_api.create_expense(12, "Foo");
 }
